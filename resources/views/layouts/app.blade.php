c8714a01b4fb20a7cb0189192d34d85221b0bf0c
lccLuffy <528360256@qq.com>
2016-02-28 19:25:23 +0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="wb:webmaster" content="4ed6b1566c93d733"/>
     <title>
         lcc_luffy
         @yield('title')
     </title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="lcc_luffy blog 博客 laravel" />
+    <meta name="author" content="lcc_luffy" />
+    <meta name="description" content=" @section('description') " />
+
 
     <!-- Fonts -->
     <link href="//cdn.bootcss.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
