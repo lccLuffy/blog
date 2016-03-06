e8555e36098e6ba4c9fb6074d40816fc77256cbd
lccLuffy <528360256@qq.com>
2016-03-06 20:02:33 +0800

    update

--- app/Http/Controllers/API/APIPostController.php
+++ app/Http/Controllers/API/APIPostController.php
@@ -7,12 +7,13 @@ use App\Http\Requests;
 use App\Post;
 use App\User;
 use Dingo\Api\Routing\Helpers;
+use Illuminate\Http\Request;
 
 class APIPostController extends BaseController
 {
     public function __construct()
     {
-        $this->middleware('api.auth',['except' => ['index']]);
+        $this->middleware('api.auth', ['except' => ['index']]);
     }
 
     /**
@@ -21,5 +22,33 @@ class APIPostController extends BaseController
     public function index()
     {
         return Post::all();
+    }
+
+    public function store(Request $request)
+    {
+        $rules = [
+            'title' => 'required',
+            'content_markdown' => 'required|min:5',
+        ];
+
+        $validator = $this->getValidationFactory()->make($request->only('title', 'content_markdown'), $rules);
+        if ($validator->fails()) {
+            abort(200, json_encode($validator->errors()));
+        }
+
+        $post = Post::create([
+            'title' => $request['title'],
+            'content_markdown' => $request['content_markdown'],
+            'content_html' => markdown2Html($request['content_markdown']),
+            'user_id' => $this->auth()->id,
+        ]);
+        if ($post)
+        {
+            return $this->wrapArray('create success', false, 'create success');
+        }
+        else
+        {
+            return $this->wrapArray('create fail', true, 'create fail');
+        }
     }
 }
